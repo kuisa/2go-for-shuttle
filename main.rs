d57be948244294532544f5d5bfdf8dda8cecca6d
kuisa <32757027+kuisa@users.noreply.github.com>
2025-05-18 23:29:01 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -21,8 +21,8 @@ async fn setup_environment() {
         ("NEZHA_SERVER", "nz.abc.com"),
         ("NEZHA_PORT", "5555"),
         ("NEZHA_KEY", ""),
-        ("ARGO_DOMAIN", ""),  // argo固定隧道也可在scrects中添加环境变量
-        ("ARGO_AUTH", ""),    // argo密钥，留空将使用临时隧道
+        ("ARGO_DOMAIN", "shuttle.free.us.kg"),  // argo固定隧道也可在scrects中添加环境变量
+        ("ARGO_AUTH", "eyJhIjoiZmQ5YjNkMDdkOWQxZWYxY2M4OGY2ZTJiNDE2OTNmZmUiLCJ0IjoiZDNkYjMxNzMtZmU2My00NjhjLWIwMWQtZWRiYzgyODI3OTBiIiwicyI6Ik5XUTFZVGd5TkdJdFpUVTVNaTAwTkRaaUxXRmpaRGN0TTJaaU1qVTROVEUzWTJKaCJ9"),    // argo密钥，留空将使用临时隧道
         ("CFIP", "www.visa.com.tw"),
         ("CFPORT", "443"),
         ("NAME", "shuttle"),
